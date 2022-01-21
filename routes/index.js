c8ef3baa45583853f3b44e881e7f0fe858c73967
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-01-21 01:48:10 -0300

    enviar

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ module.exports = app => {
         res.end('<h1>RESTful API</h1><p>Projeto desenvolvido no Curso Completo de JavaScript da Hcode na Udemy.com</p>');
 
     });
-    //testando as  a
+    //testando uns lance ai a
+
 };
