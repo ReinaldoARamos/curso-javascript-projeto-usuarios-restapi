b6e3d9619af088ebe66dfa79b90bb2ae11fcf05d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-02-09 22:14:11 -0300

    alterando a prota

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+
 const expressValidator = require('express-validator');
 
 let app = express();
@@ -11,7 +12,8 @@ app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
 
-app.listen(3000, '127.0.0.1', ()=>{
+
+app.listen( 4000, '127.0.0.1', ()=>{
 
     console.log('servidor rodando!');
 
